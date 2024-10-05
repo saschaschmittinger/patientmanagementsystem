cb58af1048666a4aac6a1187f15ec6704ca9b5a6
Codux <codux@codux.com>
2024-10-05 19:49:16 +0200

    changes

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+import React from "react";
+
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: string;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+}
+
+const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
+  return (
+    <>
+      <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <FormItem className="flex-1">
+            {fieldType !== FormFieldType.CHECKBOX && label && (
+              <FormLabel> {label} </FormLabel>
+            )}
+          </FormItem>
+        )}
+      />
+    </>
+  );
+};
+
+export default CustomFormField;
